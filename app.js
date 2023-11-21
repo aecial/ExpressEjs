0d1b73a7824ec157be907e43370c4d0a36e0ede1
Teddy Pascual <teddylayson2000@gmail.com>
2023-11-21 17:56:37 +0800

    added end point for addCrop

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const prisma = new PrismaClient();
 const availableCrops = require("./model/crops.json");
 app.listen(port, console.log(`Server is listening on port ${port}...`));
 app.set("view engine", "ejs");
-
+app.use(express.json());
 //routes
 app.get("/", (req, res) => {
   res.render("index", { availableCrops });
@@ -22,6 +22,24 @@ app.get("/users", async (req, res) => {
 app.get("/getCrops", async (req, res) => {
   const crops = await prisma.crops.findMany();
   res.json(crops);
+});
+app.post("/addCrop", async (req, res) => {
+  const { name, price } = req.body;
+  try {
+    const crops = await prisma.crops.count();
+    res.send(crops);
+  } catch (error) {
+    res.send(error);
+  }
+
+  // const newCrop = await prisma.crops.create({
+  //   data: {
+  //     name: name,
+  //     price: price,
+  //   },
+  // });
+  // res.status(201);
+  res.send({ message: crops });
 });
 // app.post("/newCrops", async (req, res) => {
 //   const newCrop = await prisma.crops.create({
